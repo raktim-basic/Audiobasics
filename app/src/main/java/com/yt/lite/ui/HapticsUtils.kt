318d36aa78c6cc7c9cfe7291eda999af845ae2a5
raktim-basic <ruma05518@gmail.com>
2026-04-13 00:01:03 +0530

    Update HapticsUtils.kt

--- app/src/main/java/com/yt/lite/ui/HapticsUtils.kt
+++ app/src/main/java/com/yt/lite/ui/HapticsUtils.kt
@@ -10,10 +10,20 @@ object HapticUtils {
     fun performSubtleHaptic(context: Context) {
         val vibrator = ContextCompat.getSystemService(context, Vibrator::class.java) ?: return
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            vibrator.vibrate(VibrationEffect.createOneShot(15, VibrationEffect.DEFAULT_AMPLITUDE))
+            vibrator.vibrate(VibrationEffect.createOneShot(20, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
             @Suppress("DEPRECATION")
-            vibrator.vibrate(15)
+            vibrator.vibrate(20)
+        }
+    }
+
+    fun performStrongHaptic(context: Context) {
+        val vibrator = ContextCompat.getSystemService(context, Vibrator::class.java) ?: return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(60, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            @Suppress("DEPRECATION")
+            vibrator.vibrate(60)
         }
     }
 }
