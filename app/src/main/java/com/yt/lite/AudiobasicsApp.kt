5388a632e24304fcea33c2cb987132c471067ab8
raktim-basic <ruma05518@gmail.com>
2026-06-03 15:11:15 +0530

    Add CipherDeobfuscator initialization in AudiobasicsApp
    Initialize CipherDeobfuscator in the application class.

--- app/src/main/java/com/yt/lite/AudiobasicsApp.kt
+++ app/src/main/java/com/yt/lite/AudiobasicsApp.kt
@@ -1,7 +1,13 @@
 package com.yt.lite
 
 import android.app.Application
+import com.yt.lite.api.cipher.CipherDeobfuscator
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AudiobasicsApp : Application()
+class AudiobasicsApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CipherDeobfuscator.initialize(this)
+    }
+}
