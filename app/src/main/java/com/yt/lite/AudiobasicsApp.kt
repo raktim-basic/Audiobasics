9d08ea30fd9fbdbb41a3d7df63789c1d25004af7
raktim-basic <ruma05518@gmail.com>
2026-06-04 18:20:17 +0530

    Update AudiobasicsApp.kt

--- app/src/main/java/com/yt/lite/AudiobasicsApp.kt
+++ app/src/main/java/com/yt/lite/AudiobasicsApp.kt
@@ -1,7 +1,9 @@
 package com.yt.lite
 
 import android.app.Application
+import android.util.Log
 import com.yt.lite.api.cipher.CipherDeobfuscator
+import com.yt.lite.ui.DebugLogCollector
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -11,13 +13,17 @@ class AudiobasicsApp : Application() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+            Timber.plant(object : Timber.DebugTree() {
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                    super.log(priority, tag, message, t)  // still goes to logcat
+                    DebugLogCollector.add(priority, tag, message + (t?.let { " | ${it.message}" } ?: ""))
+                }
+            })
         } else {
             Timber.plant(object : Timber.Tree() {
                 override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                    if (priority >= android.util.Log.WARN) {
-                        android.util.Log.println(priority, tag ?: "Audiobasics", message)
-                        t?.let { android.util.Log.e(tag ?: "Audiobasics", message, it) }
+                    if (priority >= Log.WARN) {
+                        Log.println(priority, tag ?: "Audiobasics", message)
                     }
                 }
             })
