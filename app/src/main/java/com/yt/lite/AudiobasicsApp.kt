e3d493a331647aa45f40b200c2d8939507de6fb7
raktim-basic <ruma05518@gmail.com>
2026-06-04 22:12:04 +0530

    Simplify Timber logging configuration
    Refactored Timber logging setup to always use DebugTree for logging.

--- app/src/main/java/com/yt/lite/AudiobasicsApp.kt
+++ app/src/main/java/com/yt/lite/AudiobasicsApp.kt
@@ -12,22 +12,16 @@ class AudiobasicsApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if (BuildConfig.DEBUG) {
-            Timber.plant(object : Timber.DebugTree() {
-                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                    super.log(priority, tag, message, t)  // still goes to logcat
-                    DebugLogCollector.add(priority, tag, message + (t?.let { " | ${it.message}" } ?: ""))
-                }
-            })
-        } else {
-            Timber.plant(object : Timber.Tree() {
-                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                    if (priority >= Log.WARN) {
-                        Log.println(priority, tag ?: "Audiobasics", message)
-                    }
-                }
-            })
-        }
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                super.log(priority, tag, message, t)
+                DebugLogCollector.add(
+                    priority,
+                    tag,
+                    message + (t?.let { " | ${it.message}" } ?: "")
+                )
+            }
+        })
 
         CipherDeobfuscator.initialize(this)
         Timber.d("AudiobasicsApp started")
