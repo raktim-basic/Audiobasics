78f32a8ebbbc38be1f8316be971e3ad328342369
raktim-basic <ruma05518@gmail.com>
2026-06-04 09:18:51 +0530

    Add Timber logging setup in AudiobasicsApp
    Initialize Timber for logging in debug and release modes.

--- app/src/main/java/com/yt/lite/AudiobasicsApp.kt
+++ app/src/main/java/com/yt/lite/AudiobasicsApp.kt
@@ -3,11 +3,27 @@ package com.yt.lite
 import android.app.Application
 import com.yt.lite.api.cipher.CipherDeobfuscator
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class AudiobasicsApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(object : Timber.Tree() {
+                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                    if (priority >= android.util.Log.WARN) {
+                        android.util.Log.println(priority, tag ?: "Audiobasics", message)
+                        t?.let { android.util.Log.e(tag ?: "Audiobasics", message, it) }
+                    }
+                }
+            })
+        }
+
         CipherDeobfuscator.initialize(this)
+        Timber.d("AudiobasicsApp started")
     }
 }
