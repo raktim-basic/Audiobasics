659b1ba73e00617fc2276a13258a8ddea09fde3d
raktim <ruma05518@gmail.com>
2026-07-02 19:36:34 +0530

    Add clickable import to CustomPlaylistScreen.kt

--- app/src/main/java/com/rkd/audiobasics/ui/CustomPlaylistScreen.kt
+++ app/src/main/java/com/rkd/audiobasics/ui/CustomPlaylistScreen.kt
@@ -2,6 +2,7 @@ package com.rkd.audiobasics.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
