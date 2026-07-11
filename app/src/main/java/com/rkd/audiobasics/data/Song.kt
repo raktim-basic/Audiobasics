079ced31ef23d4d2e5bedb2b24006f30b8a98f06
raktim <ruma05518@gmail.com>
2026-07-11 16:28:16 +0530

    Add albumTitle property to Song data class

--- app/src/main/java/com/rkd/audiobasics/data/Song.kt
+++ app/src/main/java/com/rkd/audiobasics/data/Song.kt
@@ -8,6 +8,7 @@ data class Song(
     val duration: Long = 0L,
     val isAlbum: Boolean = false,
     val albumId: String = "",
+    val albumTitle: String = "",
     val isCached: Boolean = false,
     val cacheFailed: Boolean = false,
     val isExplicit: Boolean = false,
