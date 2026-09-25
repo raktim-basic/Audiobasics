6bf72d9c5f8d5c00cbf78155e3310abc90da512f
raktim <262507502+raktim-basic@users.noreply.github.com>
2026-09-25 12:46:47 +0530

    Add import for isSystemInDarkTheme in MorphOverlay

--- app/src/main/java/com/rkd/audiobasics/ui/MorphOverlay.kt
+++ app/src/main/java/com/rkd/audiobasics/ui/MorphOverlay.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
